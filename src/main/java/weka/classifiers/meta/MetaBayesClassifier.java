5aedc31576d85e1e67d3cec4a27ab37b794b592f
PT <pawel.trajdos@pwr.edu.pl>
2018-06-08 16:50:52 +0200

    1. Gaussian Neighbourhood has been added. 2. Meta-bayes classifier has been implemented.

--- src/main/java/weka/classifiers/meta/MetaBayesClassifier.java
+++ src/main/java/weka/classifiers/meta/MetaBayesClassifier.java
@@ -4,8 +4,13 @@
 package weka.classifiers.meta;
 
 import weka.classifiers.Classifier;
+import weka.core.Capabilities;
+import weka.core.Capabilities.Capability;
 import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.Utils;
 import weka.tools.GlobalInfoHandler;
+import weka.tools.data.InstancesOperator;
 
 /**
  * @author pawel
@@ -17,6 +22,10 @@ public class MetaBayesClassifier extends RRCBasedWithValidation implements Globa
 	 * 
 	 */
 	private static final long serialVersionUID = -8867299051220506468L;
+	
+	protected double[] priorProbs;
+	
+	public static final double EPS = 1E-6;
 
 	/**
 	 * 
@@ -32,15 +41,66 @@ public class MetaBayesClassifier extends RRCBasedWithValidation implements Globa
 	}
 	
 	
+	
+
+	/* (non-Javadoc)
+	 * @see weka.classifiers.meta.RRCBasedWithValidation#buildClassifier(weka.core.Instances)
+	 */
+	@Override
+	public void buildClassifier(Instances arg0) throws Exception {
+		super.buildClassifier(arg0);
+		int numTrainInstances = arg0.numInstances();
+		
+		int[] counts =  InstancesOperator.objPerClass(arg0);
+		this.priorProbs  =new double[counts.length];
+		
+		for(int c=0;c<counts.length;c++)
+			this.priorProbs[c] = (double)counts[c]/numTrainInstances;
+			
+	}
 
 	/* (non-Javadoc)
 	 * @see weka.classifiers.AbstractClassifier#distributionForInstance(weka.core.Instance)
 	 */
 	@Override
 	public double[] distributionForInstance(Instance instance) throws Exception {
-		// TODO Auto-generated method stub
-		return this.m_Classifier.distributionForInstance(instance);
+		int numClasses = this.validationSet.numClasses();
+		double[] distribution = new double[numClasses];
+		double[] neighCoeffs =this.neighCalc.getNeighbourhoodCoeffs(validationSet, instance);
+		double classifierPrediction = this.m_Classifier.classifyInstance(instance);
+		
+		int classIdx = this.validationSet.classIndex();
+		double neigSum=0;
+		int valClass;
+		
+		double[] RRCProbs = new double[numClasses];
+		for(int i=0;i<neighCoeffs.length;i++) {
+			neigSum += neighCoeffs[i];
+			valClass = (int) this.validationSet.get(i).classValue();
+			RRCProbs[valClass]+= this.validationResponses.get(i)[(int)classifierPrediction] * neighCoeffs[i];
+		}
+		
+		if(Utils.eq(neigSum, 0)) {
+			return this.m_Classifier.distributionForInstance(instance);
+		}
+		
+		double normalizer =EPS ;
+		double delta = EPS/numClasses;
+		for(int c =0;c<RRCProbs.length;c++) {
+			RRCProbs[c]/=neigSum;
+			distribution[c] = this.priorProbs[c]*RRCProbs[c];
+			normalizer+=distribution[c];
+		}
+		
+		for(int c=0;c<numClasses;c++) {
+			distribution[c]+=delta;
+			distribution[c]/=normalizer;
+		}
+		
+		return distribution;
 	}
+	
+	
 
 	/**
 	 * @param args
--- src/main/java/weka/classifiers/meta/RRC/neighbourhood/DistanceBasedNeighbourhood.java
+++ src/main/java/weka/classifiers/meta/RRC/neighbourhood/DistanceBasedNeighbourhood.java
@@ -4,17 +4,24 @@
 package weka.classifiers.meta.RRC.neighbourhood;
 
 import java.io.Serializable;
+import java.util.Enumeration;
+import java.util.Vector;
 
 import weka.core.DistanceFunction;
 import weka.core.EuclideanDistance;
 import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.Option;
+import weka.core.OptionHandler;
+import weka.core.Utils;
 
 /**
  * @author pawel
  *
  */
-public class DistanceBasedNeighbourhood implements NeighbourhoodCalculator, Serializable {
+public abstract class DistanceBasedNeighbourhood implements NeighbourhoodCalculator, Serializable, OptionHandler {
+
+	
 
 	/**
 	 * 
@@ -30,14 +37,6 @@ public class DistanceBasedNeighbourhood implements NeighbourhoodCalculator, Seri
 		
 	}
 
-	/* (non-Javadoc)
-	 * @see weka.classifiers.meta.RRC.neighbourhood.NeighbourhoodCalculator#getNeighbourhoodCoeffs(weka.core.Instances, weka.core.Instance)
-	 */
-	@Override
-	public double[] getNeighbourhoodCoeffs(Instances dataset, Instance instance) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 	/**
 	 * @return the distFun
@@ -57,6 +56,78 @@ public class DistanceBasedNeighbourhood implements NeighbourhoodCalculator, Seri
 		this.distFun = distFun;
 	}
 	
+	protected double[] calculateDistances(Instances data, Instance instance) {
+		int numInstances = data.numInstances();
+		double[] distances = new double[numInstances];
+		this.distFun.setInstances(data);
+		for(int i=0;i<numInstances;i++) {
+			distances[i] = this.distFun.distance(data.get(i), instance);
+		}
+		return distances;
+	}
+	
+	/* (non-Javadoc)
+	 * @see weka.core.OptionHandler#listOptions()
+	 */
+	@Override
+	public Enumeration<Option> listOptions() {
+		Vector<Option> newVector = new Vector<Option>(1);
+		
+		 newVector.addElement(new Option(
+			      "\tThe distance function to be used"+
+		          "(default:" + EuclideanDistance.class.toGenericString()  + ").\n",
+			      "DI", 1, "-DI"));
+		 
+		return newVector.elements();
+	}
+
+
+	/* (non-Javadoc)
+	 * @see weka.core.OptionHandler#setOptions(java.lang.String[])
+	 */
+	@Override
+	public void setOptions(String[] options) throws Exception {
+		String distanceString = Utils.getOption("DI", options);
+	    if(distanceString.length() != 0) {
+	      String distanceClassSpec[] = Utils.splitOptions(distanceString);
+	      if(distanceClassSpec.length == 0) { 
+	        throw new Exception("Invalid  distance function" ); 
+	      }
+	      String className = distanceClassSpec[0];
+	      distanceClassSpec[0] = "";
+
+	      this.setDistFun( (DistanceFunction)
+	                  Utils.forName( DistanceFunction.class, 
+	                                 className, 
+	                                 distanceClassSpec)
+	                                        );
+	    }
+	    else 
+	      this.setDistFun(new EuclideanDistance());
+		
+		
+		
+	}
+
+
+	/* (non-Javadoc)
+	 * @see weka.core.OptionHandler#getOptions()
+	 */
+	@Override
+	public String[] getOptions() {
+		Vector<String> options = new Vector<String>();
+	    
+
+	    options.add("-DI");
+	    String rrcCalcOptions[] = new String[] {""};
+	    if(this.distFun instanceof OptionHandler) {
+	    	rrcCalcOptions = ((OptionHandler) this.distFun).getOptions();
+	    }
+	    options.add(this.distFun.getClass().getName()+" "+Utils.joinOptions(rrcCalcOptions)); 
+	    
+	    
+	    return options.toArray(new String[0]);
+	}
 	
 
 }
