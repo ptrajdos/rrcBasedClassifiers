3d1cad4b5481a35d7d56f9de04e6d0b296a3c15e
pawel <pawel.trajdos@pwr.edu.pl>
2020-07-02 13:14:33 +0200

    Additional tests for updateable RRCBasedWithValidation

--- src/test/java/weka/classifiers/meta/RRC/RRCBasedWithValidationTest.java
+++ src/test/java/weka/classifiers/meta/RRC/RRCBasedWithValidationTest.java
@@ -1,7 +1,10 @@
 package weka.classifiers.meta.RRC;
 
+import java.util.List;
+
 import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.Utils;
 import weka.tools.data.RandomDataGenerator;
 import weka.tools.tests.DistributionChecker;
 
@@ -35,6 +38,7 @@ public abstract class RRCBasedWithValidationTest extends RRCWrapperTest {
 	
 	public void testKeepValidationInstances() {
 		RandomDataGenerator gen = new RandomDataGenerator();
+		gen.setNumObjects(100);
 		Instances data = gen.generateData();
 		
 		
@@ -45,10 +49,15 @@ public abstract class RRCBasedWithValidationTest extends RRCWrapperTest {
 		
 		try {
 			rrcB.buildClassifier(data);
+			int valSize = rrcB.validationSet.size();
+			assertTrue("Val size", valSize == rrcB.validationResponses.size());
 			for (Instance instance: data) {
 				rrcB.updateClassifier(instance);
 				double[] respones = rrcB.distributionForInstance(instance);
 				assertTrue("Keep validation Instances. Distribution check", DistributionChecker.checkDistribution(respones));
+				assertTrue("Number of validation instances", rrcB.validationSet.size() == rrcB.validationResponses.size());
+				assertTrue("Updated validation", rrcB.validationResponses.size() == ++valSize);
+				weightsChecker(rrcB);
 			}
 		} catch (Exception e) {
 			fail("Update Keep instances" + e.toString());
@@ -56,6 +65,53 @@ public abstract class RRCBasedWithValidationTest extends RRCWrapperTest {
 		
 		
 		
+	}
+	
+	public void testValidationInstances() {
+		RandomDataGenerator gen = new RandomDataGenerator();
+		gen.setNumObjects(100);
+		Instances data = gen.generateData();
+		
+		
+		RRCBasedWithValidation rrcB = (RRCBasedWithValidation) this.getClassifier();
+		rrcB.setCrossvalidate(true);
+		rrcB.setKeepOldValidationInstances(false);
+		String[] options = rrcB.getOptions();
+		
+		try {
+			rrcB.buildClassifier(data);
+			int valSize = rrcB.validationSet.size();
+			assertTrue("Val size", valSize == rrcB.validationResponses.size());
+			for (Instance instance: data) {
+				rrcB.updateClassifier(instance);
+				double[] respones = rrcB.distributionForInstance(instance);
+				assertTrue(" validation Instances. Distribution check", DistributionChecker.checkDistribution(respones));
+				assertTrue("Number of validation instances", rrcB.validationSet.size() == rrcB.validationResponses.size());
+				assertTrue("Updated validation", rrcB.validationResponses.size() == valSize);
+				weightsChecker(rrcB);
+			}
+		} catch (Exception e) {
+			fail("Update Keep instances" + e.toString());
+		}
+		
+		
+		
+	}
+	
+	protected void weightsChecker(RRCBasedWithValidation rrc) {
+		List<Double> weights = rrc.instanceDecayWeights;
+		int size = weights.size();
+		double endWeight = weights.get(size-1);
+		// last weight should be equall one
+		assertTrue("Last weight", Utils.eq(endWeight, 1.0) );
+		
+		//older instances should get lower weights
+		for(int i=0;i<size-1;i++) {
+			if(weights.get(i) > weights.get(i+1))
+				fail("Weights are not decreasing");
+		}
+		
+	
 	}
 	
 
