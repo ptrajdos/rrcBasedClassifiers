cdb73972a79e6a829d6c28af65c971eee312987e
pawel <pawel.trajdos@pwr.edu.pl>
2020-07-31 17:41:04 +0200

    JAvadoc bugs have been fixed4.

--- src/main/java/weka/classifiers/meta/RRCEns/calculators/RRCCalcEns.java
+++ src/main/java/weka/classifiers/meta/RRCEns/calculators/RRCCalcEns.java
@@ -14,7 +14,7 @@ public interface RRCCalcEns {
 	 * Calculates RRC value for the ensemble prediction
 	 * @param ensemblePrediction -- prediction of the ensmeble [classifier number] x [class supports]
 	 * @return -- array of class-specific winning probability
-	 * @throws an exception When something goes wrong
+	 * @throws Exception an exception When something goes wrong
 	 */
 	public double[] calculateRRC(double[][] ensemblePrediction)throws Exception;
 
